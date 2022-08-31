e312c7264488c354849cca63c130b47eb01da1d7
Lucas da Costa <lucasdacosta@usp.br>
2022-08-31 02:11:18 -0300

    Forcing nox to install modules in session

--- noxfile.py
+++ noxfile.py
@@ -3,22 +3,23 @@ import nox
 
 @nox.session(python=["3.9"])
 def tests(session):
-    session.install("poetry")
-    session.run("poetry", "install")
+    session.run("pip", "install", "pytest", "--quiet")
+    session.run("pip", "install", "coverage", "--quiet")
     session.run("coverage", "run", "-m", "pytest")
     session.run("coverage", "report")
 
 
 @nox.session
-def lint(session):
-    session.install("poetry")
-    session.run("poetry", "install")
+def lint(session, reuse_venvs=True):
+    session.run_always("poetry", "install", external=True)
+    session.run("pip", "install", "black", "--quiet")
+    session.run("pip", "install", "flake8", "--quiet")
     session.run("black", "--check", ".")
-    session.run("flake8", ".")
+    session.run("flake8", "--config=.flake8", ".")
 
 
 @nox.session
-def typing(session):
-    session.install("poetry")
-    session.run("poetry", "install")
-    session.run("mypy", ".")
+def typing(session, reuse_venvs=True):
+    session.run("pip", "install", "mypy", "--quiet")
+    # session.run("poetry", "install")
+    session.run("mypy", "--config-file=mypy.ini", ".")
